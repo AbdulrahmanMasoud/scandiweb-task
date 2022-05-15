e3fa277e2620eed63eaacddb3fae779060d8261a
AbdulrahmanMasoud <abdulrahman.masoud.official@gmail.com>
2022-05-16 00:59:30 +0200

    view and response class

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -1,10 +1,12 @@
 <?php
 namespace Scandiweb\Product\Controllers;
 
+use Scandiweb\Product\Views\View;
+
 class ProductController
 {
     public function here($id)
     {
-        echo "hetre".$id;
+        return View::render("product", ['name'=>"Abdulrahman"]);
     }
 }
